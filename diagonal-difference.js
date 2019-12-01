97fcfe1dceb30f82a00f7503946ab7aeb1111a41
caio cutrim <caiocutrim.js@gmail.com>
2019-12-01 19:40:11 -0200

    Remove extra-context names

--- diagonal-difference.js
+++ diagonal-difference.js
@@ -3,8 +3,8 @@ const arr = [1, [1, 2, 3], 3, [4, 5, 6], [9, 8, 9]];
 
 function diagonalDifference(arr) {
   return Math.abs(
-    +leftDiagonalCount(setLeftDiagonalNums(setMatrixNums(arr))) - 
-    +rightDiagonalCount(setRightDiagonalNums(setMatrixNums(arr)))
+    +leftSum(setLeftNums(setMatrixNums(arr))) - 
+    +rightSum(setRightNums(setMatrixNums(arr)))
   );
 }
 
@@ -12,21 +12,21 @@ function setMatrixNums(matrixNums) {
   return arr.filter((value) => (Array.isArray(value)));
 }
 
-function setLeftDiagonalNums(matrixNums) {
+function setLeftNums(matrixNums) {
   return matrixNums.map((value, i, arr) => (arr[i][i]));
 }
 
-function setRightDiagonalNums(matrixNums) {
+function setRightNums(matrixNums) {
   return matrixNums.map((value, i, arr) => (arr[i][arr[i].length - i - 1]));
 }
 
-function leftDiagonalCount(leftDiagonalNums) {
+function leftSum(leftDiagonalNums) {
   return leftDiagonalNums.reduce((acc, num) => (acc + num ));
 }
 
-function rightDiagonalCount(rightDiagonalNums) {
+function rightSum(rightDiagonalNums) {
   return rightDiagonalNums.reduce((acc, num) => (acc + num ));
 }
 
-// console.log(diagonalDifference(arr));
+console.log(diagonalDifference(arr));
 // diagonalDifference(arr);
